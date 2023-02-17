ae8d6ab0e60baf0526fd82d8849f1f64800027f8
Esianyo <esianicd@gmail.com>
2023-02-16 23:22:40 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fwrite(4 " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
