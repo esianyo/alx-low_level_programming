ebde4253cd87f07dfc1b3cb258001665e2e4ef56
Esianyo <esianicd@gmail.com>
2023-02-18 14:52:41 -0800

    Printing alphabets in uppercase and lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	int lower_case;
-	int upper_case;
+	char lower_case;
+	char upper_case;
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
@@ -17,7 +17,7 @@ int main(void)
 	}
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
-		putchar((upper_case);
+		putchar(upper_case);
 	}
 {
 	putchar("\n");
