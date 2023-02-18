05eaf3530316c031e0bfc45be3f6f539751a4b01
Esianyo <esianicd@gmail.com>
2023-02-17 16:07:34 -0800

    Determining whether positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Main function
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if
+		(n > 0);
+		printf(n, "%d is a positive.");
+	else
+		printf(n, "%d is negative.");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Decisions in C language
