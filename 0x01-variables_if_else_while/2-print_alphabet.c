666c67254d42a39ee9795ff5ceb8984e9f4117b5
Esianyo <esianicd@gmail.com>
2023-02-18 15:35:28 -0800

    Printing lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,6 @@ int main(void)
 	char lower_case;
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-		putchar(lower_case);
-	putchar('\n');
+		putchar(lower_case, '\n');
 	return (0);
 }
