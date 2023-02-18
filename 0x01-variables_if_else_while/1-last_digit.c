0e6e363252b80aa181c290fe5bcb4583f5f01ae3
Esianyo <esianicd@gmail.com>
2023-02-17 18:04:54 -0800

    Printing last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,8 @@ int main(void)
 		printf("Last digit of %d\n is %d\n and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("Last digit of %d\n is %d\n and is 0\n", n, last);
-	else if (last < 6 && != 0)
-		printf("Last digit of %d\n is %d\n and is less than 6 and not 0\n");
+	else if (last < 6 && last != 0)
+		printf("Last digit of %d\n is %d\n and is less than 6 and not 0\n", n, last);
 	else
 		printf("Try again");
 	return (0);
