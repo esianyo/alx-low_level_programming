1b3545b7e62a6856475f083bcdbb5cdc7efde31c
Esianyo <esianicd@gmail.com>
2023-02-17 18:59:28 -0800

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+		putchar('\n');
+	}
+	return (0);
+}
