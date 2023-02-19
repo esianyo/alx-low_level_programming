b52c18a40936854ee686ac81099f7c0fc89bff72
Esianyo <esianicd@gmail.com>
2023-02-18 16:27:44 -0800

    Skip e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,10 @@ int main(void)
 	char qNe;
 
 	for (qNe = 'a'; qNe <= 'z'; qNe++)
-		putchar("%deq", qNe);
+	{
+		if (qNe != 'e' && qNe != 'q')
+			putchar(qNe);
+	}
 	putchar('\n');
 	return (0);
 }
