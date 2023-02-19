441d268800ee5494bbf6a6db6b79ce552281342e
Esianyo <esianicd@gmail.com>
2023-02-18 18:06:39 -0800

    Alphabets in reverse form

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char reverse;
 
-	for (reverse >= 'a'; reverse = 'z'; reverse--)
+	for (reverse <= 'z'; reverse = 'a'; reverse--)
 		putchar(reverse);
 	putchar('\n');
 	return (0);
