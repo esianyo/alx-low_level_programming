93d716bc653f97eac4aded7da0dc1eff378c800a
Esianyo <esianicd@gmail.com>
2023-02-18 19:04:39 -0800

    all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,14 @@ int main(void)
 	int comb;
 
 	for (comb = 0; comb <= 9; comb++)
+	{
 		putchar('0' + comb);
-	putchar(",");
-	putchar(" ");
+		if (comb == 9)
+			continue;
+		putchar(",");
+		putchar(' ');
+	}
+
 	putchar('\n');
 	return (0);
 }
