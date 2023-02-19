8b7fedeae120b29b56692b90647607f2db870c4c
Esianyo <esianicd@gmail.com>
2023-02-18 17:57:23 -0800

    Alphabets in reverse form

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char reverse;
+
+	for (reverse = 'z'; reverse <= 'a'; reverse++)
+		putchar(reverse);
+	putchar('\n');
+	return (0);
+}
