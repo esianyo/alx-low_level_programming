8001304bea5680110e66bb6f49f942b05ad56c77
Esianyo <esianicd@gmail.com>
2023-02-27 14:14:39 -0800

    Changing value of pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,11 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: parameter to be checked
  *
  * Return: Always 0
  */
 void reset_to_98(int *n)
 {
-	int a;
-
-	a = 98;
-	n = &a;
+	*n = 98;
 }
