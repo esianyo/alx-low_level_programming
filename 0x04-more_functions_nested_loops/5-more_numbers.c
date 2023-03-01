fc9f9e4da1f1c4e522ac0ab9b04d40198269111d
Esianyo <esianicd@gmail.com>
2023-02-28 20:23:32 -0800

    Prints more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,14 @@ void more_numbers(void)
 {
 	int num, countr;
 
-	for (countr = 0; countr <= 9; countr++ )
+	for (countr = 0; countr <= 10; countr++ )
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
-				_putchar('0' + num / 10);
+			if (num >= 10)
+				_putchar('1');
+			_putchar('0' + (num % 10));
 		}
-		_putchar('0' + (num % 10));
+		_putchar('\n'
 	}
-	_putchar('\n');
 }
