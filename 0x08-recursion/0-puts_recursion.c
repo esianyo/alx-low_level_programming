2ca1f0e8149520b326cbe3bc6c182f7a9d3260bf
Esianyo <esianicd@gmail.com>
2023-03-08 15:16:18 -0800

    Pushing header file, readme and first task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: character variable
+ */
+
+void _puts_recursion(char *s)
+{
+	char *s[50];
+
+	_putchar("%s", s);
+}
