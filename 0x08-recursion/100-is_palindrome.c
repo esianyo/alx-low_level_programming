0bfb63fee65528ecf97f2972b4683879067ad955
Esianyo <esianicd@gmail.com>
2023-03-09 16:57:12 -0800

    Checks if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,20 +3,45 @@
 
 /**
  * is_palindrome - checks for a palindrome string
+ * @begin: beginning of index
+ * @stop: ending of index
+ *
+ * Return: returns 1 if palindrome
+ */
+
+int assistant(char *s, int begin, int stop)
+{
+	if (begin >= stop)
+	{
+		return (1);
+	}
+	else if (s[begin] != s[stop])
+	{
+		return (0);
+	}
+	else
+	{
+		return assistant(s, begin + 1, stop - 1);
+	}
+}
+
+
+/**
+ * is_palindrome - checks for a palindrome string
  * @s: variable for string
  *
  * Return: returns 1 if palindrome
  */
+
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
-
-	for (int i = 0; i < len / 2; i++)
+	
+	if (len == 0)
 	{
-		if (s[i] != s[len - i - 1])
-		{
-			return (0);
-		}
+		return 1;
 	}
-	return (1);
+	else
+	{
+		return assistant(s, 0, len - 1);}
 }
