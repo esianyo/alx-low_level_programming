d4ffe598c560554d082b9b6a82bbcf0e6b806cb8
Esianyo <esianicd@gmail.com>
2023-03-11 12:01:40 -0800

    prints all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,8 +12,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 1; i < argc; i++)
-		printf("%s\n", argv[i]);
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
 
 	return 0;
 }
