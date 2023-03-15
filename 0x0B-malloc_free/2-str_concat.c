99b423749f201cafd4d57a56f85148e4c1534f44
Esianyo <esianicd@gmail.com>
2023-03-14 17:29:19 -0700

    a function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: initial string
+ * @s2: duplicated string
+ *
+ * Return: outcome
+ */
+
+char* str_concat(char *s1, char *s2)
+{
+	char *result;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	result = strdup(s1);
+	
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	result = realloc(result, strlen(s1) + strlen(s2) + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	strcat(result, s2);
+
+	return (result);
+}
