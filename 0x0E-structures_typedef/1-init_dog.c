99b9e2cd16972170190bc9da55d451128888385f
Esianyo <esianicd@gmail.com>
2023-03-20 19:37:25 -0700

    initializing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -19,3 +19,25 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
+
+/**
+ * main - entry point
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	struct dog my_dog;
+	char *name = "Spiky";
+	float age = 11;
+	char *owner = "Esianyo";
+
+	init_dog(&my_dog, name, age, owner);
+
+	init_dog(&my_dog, name, age, owner);
+
+	printf("My dog's name is %s.\n", my_dog.name);
+	printf("My dog is %.1f years old.\n", my_dog.age);
+	printf("My dog's owner is %s.\n", my_dog.owner);
+}
