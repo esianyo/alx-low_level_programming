8c8a08420a9b79cd0c4cb3f4f8daa4cb9f43ede5
Esianyo <esianicd@gmail.com>
2023-03-20 18:02:28 -0700

    structure dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure for dog
+ * @name: pointer for name
+ * @age: dog's age
+ * @owner: who owns it
+ *
+ * Description: about the dog
+ */
+
 struct dog
 {
 	char *name;
