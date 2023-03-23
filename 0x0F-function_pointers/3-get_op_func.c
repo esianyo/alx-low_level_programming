6a95b494d4cdb2bb259b9052c1698556bbda3823
Esianyo <esianicd@gmail.com>
2023-03-22 21:25:48 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*ops[i].op == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
