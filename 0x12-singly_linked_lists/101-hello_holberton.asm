51a5eccf525f453c6911d183b5e96623fbbc869d
Esianyo <esianicd@gmail.com>
2023-03-31 22:50:03 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,31 @@
+global _start
+
 section .data
-	 message db 'Hello, Holberton', 0
-	format db '%s\n', 0
+    message db "Hello, Holberton",0
+    format db "%s\n",0
 
 section .text
-	global main
-	extern printf
+_start:
+    mov rax, 0        ; clear rax register
+    lea rdi, [message] ; load message address into rdi register
+    mov rsi, format   ; load format string into rsi register
+    xor rdx, rdx      ; clear rdx register
+    call printf       ; call printf function
+    mov rax, 60       ; load exit syscall number into rax
+    xor rdi, rdi      ; clear rdi register
+    syscall           ; call exit syscall
 
-main:
-	push message
-	push format
-	call printf
-	add rsp, 16
 
-	mov eax, 0
-	ret
+
+
+
+
+
+
+
+
+
+
+
+
+
