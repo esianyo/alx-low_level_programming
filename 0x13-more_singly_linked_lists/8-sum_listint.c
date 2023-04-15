173eb9719d3cbc97fa0bc25e224efcceb99940d1
Esianyo <esianicd@gmail.com>
2023-04-14 18:34:48 -0700

    a function that returns the sum of node of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sun of all data in a list
+ * @head: head pointer to a list
+ *
+ * Return: sum of all data
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *hold;
+	int sum = 0;
+
+	hold = head;
+
+	while (temp != NULL)
+	{
+		sum += hold->n;
+		hold = hold->next;
+	}
+
+	return (sum);
+}
