40732e47c016ad9db733da7ecb8e7f37356c36a4
Esianyo <esianicd@gmail.com>
2023-04-24 15:11:43 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
 
 int main(void)
 {
-	char *sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, sentence, 59);
+	write(2, quote, 59);
 
 	return (1);
 }
