e0466901f2718a7224d70845af5169d31bc0f068
esianyo <esianicd@gmail.com>
2023-07-26 21:48:27 +0000

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+    grid (list of list of integers): The 2D grid representing the island.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                # Check neighbors and subtract the shared edges
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
